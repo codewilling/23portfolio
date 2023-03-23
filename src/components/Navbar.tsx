cff98a00298258d592bdeeb2fbc82183976c932f
codewilling <codewilling@gmail.com>
2023-03-22 21:24:27 -0700

    add naming

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,34 +1,20 @@
-import React, { useState } from 'react'
-// import { useSpring, N from 'react-spring'
-import  InPic from '../assets/In-White-34px-R.png'
-import githubPic from '../assets/GitHub-Mark-Light-32px.png'
-export const Navbar = () => {
-const [menuTogle, setMenuToggle] = useState(false)
+import { PropsWithChildren, ReactEventHandler, useState } from "react";
+import NavToggle from "./NavToggle";
 
-    return (
-        <nav >
-            <div className="logo">
-                <h4>Sean Flores</h4>
-            </div>
-            <ul className='nav-links'>
-                <li>
-                    <a href="https://codewilling.github.io/resume/" target="_blank" rel="noopener noreferrer">Resume</a>
-                </li>
-                <li>
-                    <a href="https://www.linkedin.com/in/sean-flores" target="_blank" rel="noopener noreferrer"><img src={InPic}
-                        alt="codewilling's linkedin" /></a>
-                </li>
-                <li>
+type OptionalProps = {
+  text?: string;
+};
+export default function Navbar(props: PropsWithChildren<OptionalProps>) {
 
-                    <a href="https://www.github.com/codewilling" target="_blank" rel="noopener noreferrer"><img src={githubPic}
-                        alt="codewilling's github" /></a>
-                </li>
-            </ul>
-            <div className="burger" onClick={menuToggle => setMenuToggle(!menuToggle)}>
-                <div className='line1'></div>
-                <div className='line2'></div>
-                <div className='line3'></div>
-            </div>
-        </nav>
-    )
+    const [listClass, setListClass] = useState("navbar-list")
+    function handleToggleClick () {
+        setListClass(listClass==="navbar-list"? "navbar-list active":"navbar-list")
+  };
+  return (
+    <nav className="navbar">
+      <div className="naming"><p>Sean Flores</p></div>
+      <ul className={listClass}>{props.children}</ul>
+      <NavToggle handleToggleClick={handleToggleClick}></NavToggle>
+    </nav>
+  );
 }
