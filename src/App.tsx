078da8309730f9af2ad9509ff283969b67de3587
codewilling <codewilling@gmail.com>
2023-03-22 21:24:27 -0700

    add naming

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import "./App.css";
-import { Navbar } from "./components/Navbar";
 import { Main } from "./components/Main";
 import { Projects } from "./data/projectList";
 import { Card } from "./components/Card.js";
-import NB2 from "./components/NB2";
+import Navbar from "./components/Navbar";
 import NavItem from "./components/NavItem";
 import inPic from "./assets/In-White-34px-R.png";
 import githubPic from "./assets/GitHub-Mark-Light-32px.png";
@@ -13,7 +12,8 @@ import resume from "./assets/resume-icon-white.png";
 function App() {
   return (
     <>
-      <NB2>
+        
+      <Navbar>
         <NavItem link="https://codewilling.github.io/resume/" image={resume}>
           Resume
         </NavItem>
@@ -25,7 +25,7 @@ function App() {
           link="https://www.github.com/codewilling"
           image={githubPic}
         ></NavItem>
-      </NB2>
+      </Navbar>
       <Main />
       {/* <Opener /> */}
       <div className="projectContainer">
