7a7388edd94546ad6f4f2b6edfdb0bd151cefefa
codewilling <codewilling@gmail.com>
2023-06-08 13:48:41 -0700

    add react timer on vercel link

--- src/data/projectList.ts
+++ src/data/projectList.ts
@@ -1,65 +1,59 @@
-import * as tents from '../assets/Tents.png'
+import * as tents from "../assets/Tents.png";
 export const Projects = [
-    {   
-        name: 'Campgrounds',
-        link:'https://secure-meadow-48144.herokuapp.com/',
-        skills: [
-            'Node', 'Express', 'MongoDB', 'HTML', 'CSS', 'JavaScript', 'Responsive Design'
-        ], 
-        image: tents
-    },
-    {   
-        name: 'Sound Board',
-        link:'https://codewilling.github.io/sound-board/',
-        skills: [
-            'React', 'HTML', 'CSS', 'JavaScript', 'Responsive Design'
-        ],
-        
-        image: tents
-    },
-    {   
-        name: 'Color Guessing Game',
-        link:'https://codewilling.github.io/color-guessing-game/',
-        skills: [
-            'React', 'HTML', 'CSS', 'JavaScript', 'Responsive Design'
-        ],
-        
-        image: tents
-    },
-    {   
-        name: 'React Timer',
-        link:'https://codewilling-react-timer.herokuapp.com/',
-        skills: [
-            'React', 'HTML', 'CSS', 'JavaScript', 'Responsive Design'
-        ],
-        
-        image: tents
-    },
-    {   
-        name: 'Civic Tech Index',
-        link:'http://civictechindex.org/organizations',
-        skills: [
-            'React', 'HTML', 'CSS', 'JavaScript', 'Responsive Design'
-        ],
-        
-        image: tents
-    },
-    {   
-        name: 'Angular Heroes',
-        link:'https://codewilling-heroes.netlify.app',
-        skills: [
-            'React', 'HTML', 'CSS', 'JavaScript', 'Responsive Design'
-        ],
-        
-        image: tents
-    },
-    {   
-        name: 'React 18 Tic-Tac-Toe',
-        link:'https://genuine-sorbet-cff636.netlify.app',
-        skills: [
-            'React', 'HTML', 'CSS', 'JavaScript', 'Responsive Design'
-        ],
-        
-        image: tents
-    },
-]
+  {
+    name: "Campgrounds",
+    link: "https://secure-meadow-48144.herokuapp.com/",
+    skills: [
+      "Node",
+      "Express",
+      "MongoDB",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Responsive Design",
+    ],
+    image: tents,
+  },
+  {
+    name: "Sound Board",
+    link: "https://codewilling.github.io/sound-board/",
+    skills: ["React", "HTML", "CSS", "JavaScript", "Responsive Design"],
+
+    image: tents,
+  },
+  {
+    name: "Color Guessing Game",
+    link: "https://codewilling.github.io/color-guessing-game/",
+    skills: ["React", "HTML", "CSS", "JavaScript", "Responsive Design"],
+
+    image: tents,
+  },
+  {
+    name: "React Timer",
+    link: "https://timer-theta-seven.vercel.app/",
+    skills: ["React", "HTML", "CSS", "JavaScript", "Responsive Design"],
+
+    image: tents,
+  },
+  {
+    name: "Civic Tech Index",
+    link: "http://civictechindex.org/organizations",
+    skills: ["React", "HTML", "CSS", "JavaScript", "Responsive Design"],
+
+    image: tents,
+  },
+  {
+    name: "Angular Heroes",
+    link: "https://codewilling-heroes.netlify.app",
+    skills: ["React", "HTML", "CSS", "JavaScript", "Responsive Design"],
+
+    image: tents,
+  },
+  {
+    name: "React (v18) Tic-Tac-Toe",
+    link: "https://genuine-sorbet-cff636.netlify.app",
+    skills: ["React", "HTML", "CSS", "JavaScript", "Responsive Design"],
+
+    image: tents,
+  },
+];
